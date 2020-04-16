e1bfc0a6a5c84eaa12ae939147de04debe6b3df1
JakubMęnski <56916766+JamesManningR@users.noreply.github.com>
2020-04-16 15:00:39 +0100

    Added the vuex for projects

--- src/views/project/Index.vue
+++ src/views/project/Index.vue
@@ -1,27 +1,27 @@
 <template>
   <div class="projects">
     <h1>Projects</h1>
-    <!-- <ProjectPost
+    <ProjectPost
       v-for="project in projects"
       :key="project.id"
       :projectTitle="project.title"
-    /> -->
+    />
   </div>
 </template>
 
 <script>
-// import ProjectPost from "@/components/ProjectShow.vue";
+import ProjectPost from "@/components/ProjectShow.vue";
 import { mapState } from 'vuex'
 
 export default {
   components: {
-    // ProjectPost
+    ProjectPost
   },
   created(){
     this.$store.dispatch('project/fetchProjects')
   },
   computed:{
-    ...mapState(['event', 'user'])
+    ...mapState('project', ['projects'])
   }
 };
 </script>
