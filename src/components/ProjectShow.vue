eb6c0963217f65e0c872bc1ea0382b920fc2f76c
JakubMęnski <56916766+JamesManningR@users.noreply.github.com>
2020-04-16 12:33:01 +0100

    Added post show component

---
+++ src/components/ProjectShow.vue
@@ -0,0 +1,17 @@
+<template>
+  <figure>
+    <h2>{{projectTitle}}</h2>
+  </figure>
+</template>
+
+<script>
+export default {
+  props: {
+    projectTitle: String
+  }
+}
+</script>
+
+<style>
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,36 @@
 <template>
   <div class="home">
-
+    <ProjectPost projectTitle="Happy" />
+    <ProjectPost projectTitle="Happy" />
+    <ProjectPost projectTitle="Happy" />
   </div>
 </template>
 
 <script>
+import ProjectPost from '@/components/ProjectShow.vue';
 
 export default {
   name: "Home",
   components: {
+    ProjectPost
+  },
+  data() {
+    return {
+      projects: [
+        {
+          title: 'This is a post'
+        },
+        {
+          title: 'This Multiple Post'
+        },
+        {
+          title: 'Stop Banging outside my door'
+        },
+        {
+          title: 'Post about me'
+        },
+      ]
+    }
   }
 };
 </script>
