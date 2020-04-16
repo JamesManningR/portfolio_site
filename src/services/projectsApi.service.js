c32388e12c60facb50f96770fe48db22f8539b18
JakubMęnski <56916766+JamesManningR@users.noreply.github.com>
2020-04-16 14:15:36 +0100

    Created mock api for testing

---
+++ src/services/projectsApi.service.js
@@ -0,0 +1,18 @@
+import axios from 'axios';
+
+const mockApi = axios.create({
+  baseURL: 'http://localhost:3000',
+  headers: {
+    Accept: 'application/json',
+    'Content-Type': 'application/json'
+  }
+})
+
+export default {
+  getProjects() {
+    return mockApi.get('')
+  },
+  postProject(project) {
+    return mockApi.post('/project', project)
+  }
+}
