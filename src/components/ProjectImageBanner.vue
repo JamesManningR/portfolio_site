eb111ea46d9c1bd62d7f35169ee4b59c2f75c38f
JakubMęnski <56916766+JamesManningR@users.noreply.github.com>
2020-04-16 12:42:33 +0100

    Added Image Banner Component

---
+++ src/components/ProjectImageBanner.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="imageBanner">
+    <img class="imageBanner__image" v-for="(image, index) in images" :key="index" :src="image.src" :alt="image.altText">
+  </div>
+</template>
+
+<script>
+export default {
+  props:{
+    images: Array
+  }
+}
+</script>
+
+<style>
+
+</style>
