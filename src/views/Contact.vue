68d25297ca775c399352bb78c46ef1c447b9c696
JakubMęnski <56916766+JamesManningR@users.noreply.github.com>
2020-04-18 13:01:54 +0100

    Added contact link

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -2,6 +2,9 @@
   <main>
     <h1>Contact Me</h1>
     <ul>
+      <li>Email: <svg v-svg symbol="icon-linkedin"></svg></li>
+      <li>Linked In: <svg v-svg symbol="icon-linkedin"></svg></li>
+      <li>Linked In: <svg v-svg symbol="icon-linkedin"></svg></li>
       <li>Linked In: <svg v-svg symbol="icon-linkedin"></svg></li>
     </ul>
   </main>
