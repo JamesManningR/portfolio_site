a6c78a51f33ea71e648416618aba48bccdf98333
JakubMęnski <56916766+JamesManningR@users.noreply.github.com>
2020-04-20 22:24:26 +0100

    Added multiple file uploads

--- src/components/FileUploader.vue
+++ src/components/FileUploader.vue
@@ -1,5 +1,15 @@
 <template>
-  <input type="file" :disabled="disabled" @change="handleChange($event)" />
+  <div class="fileUploader">
+    <ul>
+      <li v-for="(file, index) in files" :key="index">{{ file.name }}</li>
+    </ul>
+    <input
+      type="file"
+      multiple
+      :disabled="disabled"
+      @change="handleChange($event)"
+    />
+  </div>
 </template>
 
 <script>
@@ -7,20 +17,31 @@ import firebaseStorage from "@/services/firebaseStorage.service";
 
 export default {
   methods: {
-    handleChange(event) {
-      this.disabled = true;
-      return firebaseStorage
-        .upload(event.target.files[0], event.target.files[0].name)
-        .then(() => {
-          this.disabled = false;
-        })
-        .catch(err => {
-          console.log(err);
-        });
+    handleChange(evt) {
+      // this.disabled = true;
+      // return firebaseStorage
+      //   .upload(event.target.files[0], event.target.files[0].name)
+      //   .then(() => {
+      //     this.disabled = false;
+      //   })
+      //   .catch(err => {
+      //     console.log(err);
+      //   });
+      this.files = evt.target.files;
+      this.files.forEach(file => {
+        console.log(file);
+        firebaseStorage
+          .upload(file)
+          .then(() => {
+            console.log("Uploaded");
+          })
+          .catch(err => console.log(err));
+      });
     }
   },
   data() {
     return {
+      files: [],
       disabled: false
     };
   }
