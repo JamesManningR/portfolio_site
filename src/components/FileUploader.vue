82b7ff735b967e9d4ce96c6cd762f73204263388
JakubMęnski <56916766+JamesManningR@users.noreply.github.com>
2020-04-24 00:45:34 +0100

    Fixed merge errors

--- src/components/FileUploader.vue
+++ src/components/FileUploader.vue
@@ -1,27 +1,18 @@
 <template>
   <div class="fileUploader">
     <ul class="fileUploader__uploaded">
-<<<<<<< HEAD
-      <li v-for="(file, index) in images.files" :key="index">
-=======
       <li
         v-for="(file, index) in settings.files"
         :key="index"
         @click="this.settings.featured = file"
       >
->>>>>>> change/remove_relational
         <label>
           <img class="fileUploader__img" :src="file.fileUrl" :alt="file.name" />
           <input
             type="radio"
-<<<<<<< HEAD
-            v-model="featured"
-            :value="file.fileUrl"
-=======
             v-model="settings.featured"
             :value="file.fileUrl"
             :id="file.name"
->>>>>>> change/remove_relational
             name="featured"
           />
         </label>
@@ -42,7 +33,6 @@
 
 <script>
 import firebaseStorage from "@/services/firebaseStorage.service";
-import firebaseApi from "@/services/firebaseApi.service";
 
 export default {
   methods: {
@@ -54,15 +44,7 @@ export default {
           .then(fileUrl => {
             // Upon success
             // push file data onto the files array
-<<<<<<< HEAD
-            const dbData = { src: fileUrl, name: file.name };
-            return firebaseApi.postMedia(dbData).then(res => {
-              this.images.files.push({ fileUrl, name: file.name });
-              this.images.uploadIds.push(res.id);
-            });
-=======
             this.settings.files.push({ fileUrl, name: file.name });
->>>>>>> change/remove_relational
           })
           .catch(err => console.log(err)); // catch errors
       });
@@ -70,35 +52,20 @@ export default {
   },
   data() {
     return {
-<<<<<<< HEAD
-      images: {
-        featured: "",
-        files: [],
-        uploadIds: []
-=======
       settings : {
         featured: "",
         files: []
->>>>>>> change/remove_relational
       },
       disabled: false
     };
   },
   watch: {
-<<<<<<< HEAD
-    uploadIds: function() {
-      this.$emit("fileUploaded", {
-        fileIds: this.images.uploadIds,
-        featured: this.images.featured
-      });
-=======
     settings: {
       deep: true,
       handler() {
         console.log('this jhappened')
         this.$emit("fileUploaded", this.settings);
       }
->>>>>>> change/remove_relational
     }
     
   }
