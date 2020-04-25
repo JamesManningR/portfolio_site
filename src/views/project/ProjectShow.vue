571e2eb0386f2698c7515e9e147a942d0858a01d
JakubMęnski <56916766+JamesManningR@users.noreply.github.com>
2020-04-25 17:35:25 +0100

    Added image banner into project show

--- src/views/project/ProjectShow.vue
+++ src/views/project/ProjectShow.vue
@@ -1,15 +1,18 @@
 <template>
   <main class="project">
+    <project-image-banner :images="project.project.images" />
     <blog-text :project="project.project" />
   </main>
 </template>
 
 <script>
+import ProjectImageBanner from "@/components/ProjectImageBanner.vue";
 import BlogText from "@/components/BlogText.vue";
 import { mapState } from "vuex";
 
 export default {
   components: {
+    ProjectImageBanner,
     BlogText
   },
   computed: {
