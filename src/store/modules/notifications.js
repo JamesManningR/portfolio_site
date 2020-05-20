7b3d95051c68e752569883225a202e457acc43ac
JamesManningR <56916766+JamesManningR@users.noreply.github.com>
2020-05-20 01:11:26 +0100

    Boilerplate for notifications

---
+++ src/store/modules/notifications.js
@@ -0,0 +1,16 @@
+import db from "@/services/nodeApi.service.js";
+import router from "@/router";
+
+export const namespaced = true;
+
+export const state = {
+};
+
+export const mutations = {
+};
+
+export const actions = {
+};
+
+export const getters = {
+};
